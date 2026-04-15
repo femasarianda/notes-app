fd9c4fdcaba62f9e31e3d8d6c6a333dce261194a
femasarianda <human.earth1606@gmail.com>
2026-04-15 23:14:18 +0700

    fix cors

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,19 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://notes-app-client-ruby.vercel.app'
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow Postman, curl
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
   credentials: true,
 }));
 app.use(morgan('dev'));
